22b1cea30a53e5960cc39deab38608a03aab90af
joyghatak <joyghatak0099@gmail.com>
2025-09-19 14:00:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const container = document.querySelector(".container");
+const seats = document.querySelectorAll(".row .seat:not(.sold)");
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+const bookNowBtn = document.getElementById("book-now-btn");
+
+let ticketPrice = localStorage.getItem("selectedMoviePrice") || 0;
+
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll(".row .seat.selected");
+  const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+
+  const selectedSeatsCount = selectedSeats.length;
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) seat.classList.add("selected");
+    });
+  }
+}
+
+container.addEventListener("click", e => {
+  if (e.target.classList.contains("seat") && !e.target.classList.contains("sold")) {
+    e.target.classList.toggle("selected");
+    updateSelectedCount();
+  }
+});
+
+// ✅ SAVES THE FINAL TOTAL ON CLICK
+bookNowBtn.addEventListener('click', () => {
+    localStorage.setItem('bookingTotal', total.innerText);
+});
+
+
+populateUI();
+updateSelectedCount();
